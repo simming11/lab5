e31c22fbcb1e47101be940dd7720453d4018452b
simming11 <abdulazem.m13@gmail.com>
2023-12-22 09:03:53 +0700

    first commit

---
+++ lib/pages/foodpage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Foodpage extends StatefulWidget {
+  const Foodpage({super.key});
+
+  @override
+  State<Foodpage> createState() => _FoodpageState();
+}
+
+class _FoodpageState extends State<Foodpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("food164"),
+      ),
+      body: const Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
